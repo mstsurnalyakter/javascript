9f04d8a4234adcc4c77a7ef377ab37d4a249b0d1
mstsurnalyakter <surnalyms@gmail.com>
2024-01-27 14:43:07 +0600

    if else condition

--- even_odd.js
+++ even_odd.js
@@ -1,5 +1,6 @@
 const num1 = 30;
 const num2 = 31;
+const num3 = 0;
 
 if (num1 % 2 === 0) {
     console.log(`Even number`);
@@ -11,4 +12,12 @@ if (num2 % 2 === 0) {
     console.log(`Even number`);
 }else{
     console.log(`Odd number`);
+}
+
+if (num3 % 2 === 0) {
+    console.log(`Even number`);
+}else if (num3 % 2 !== 0) {
+    console.log(`Odd number`);
+}else{
+    console.log(`Zero number`);
 }
