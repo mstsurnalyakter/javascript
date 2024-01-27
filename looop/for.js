ded16f2bae7263592083f9816a675c06fb3b77d5
mstsurnalyakter <surnalyms@gmail.com>
2024-01-27 17:43:42 +0600

    comment up code

--- looop/for.js
+++ looop/for.js
@@ -1,26 +1,26 @@
 // 10 times print Bangladesh
-// for (let i = 0; i <= 10; i++) {
-//     console.log(`Bangladesh`);
-// }
+for (let i = 0; i <= 10; i++) {
+    console.log(`Bangladesh`);
+}
 // print 1 - 10
-// for (let i = 1; i <= 10; i++) {
-//     console.log(i);
-// }
+for (let i = 1; i <= 10; i++) {
+    console.log(i);
+}
 
 // print 1 - 10 odd number
-// for (let i = 1; i < 10; i = i + 2) {
-//     console.log(i);
-// }
+for (let i = 1; i < 10; i = i + 2) {
+    console.log(i);
+}
 
 // print 1 - 10 even number
-// for (let i = 2; i <= 10; i = i + 2) {
-//     console.log(i);
-// }
+for (let i = 2; i <= 10; i = i + 2) {
+    console.log(i);
+}
 
 // 10,9,8,7,6,5,4,3,2,1
-// for (let i = 10; i >= 1; i--) {
-// console.log(i);
-// }
+for (let i = 10; i >= 1; i--) {
+console.log(i);
+}
 
 // 1+2+3+4+5+6+7+8+9+10
  let sum = 0;
