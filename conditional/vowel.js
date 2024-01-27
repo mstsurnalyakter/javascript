4a51803c57817437a1a698a54084aedf98323c3e
mstsurnalyakter <surnalyms@gmail.com>
2024-01-27 17:00:40 +0600

    vowel identify

--- conditional/vowel.js
+++ conditional/vowel.js
@@ -8,3 +8,25 @@ if (letter === "a" || letter === "e" || letter === "i" || letter === "o" || lett
 }else{
     console.log(`Consonant`);
 }
+
+switch (letter) {
+    case "a":
+    console.log('Vowel');
+        break;
+    case "e":
+    console.log('Vowel');
+        break;
+    case "i":
+    console.log('Vowel');
+        break;
+    case "o":
+    console.log('Vowel');
+        break;
+    case "u":
+    console.log('Vowel');
+        break;
+
+    default:
+        console.log(`Consonant`);
+        break;
+}
