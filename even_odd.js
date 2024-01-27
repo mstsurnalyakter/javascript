40f12a4dd2dc1ba89ad3d11e59fb1b0468ffcaa6
mstsurnalyakter <surnalyms@gmail.com>
2024-01-27 14:40:22 +0600

    using if-else condition check even or odd number

---
+++ even_odd.js
@@ -0,0 +1,14 @@
+const num1 = 30;
+const num2 = 31;
+
+if (num1 % 2 === 0) {
+    console.log(`Even number`);
+}else{
+    console.log(`Odd number`);
+}
+
+if (num2 % 2 === 0) {
+    console.log(`Even number`);
+}else{
+    console.log(`Odd number`);
+}
