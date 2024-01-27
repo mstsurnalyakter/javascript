f9af103f97bde53f43b0f279683c64dbce485d7a
mstsurnalyakter <surnalyms@gmail.com>
2024-01-27 15:02:22 +0600

    logical operator

---
+++ vowel.js
@@ -0,0 +1,10 @@
+let letter = prompt(`Enter a letter`);
+
+letter = letter.toLowerCase();
+
+if (letter === "a" || letter === "e" || letter === "i" || letter === "o" || letter === "u") {
+    console.log(`Vowel`);
+
+}else{
+    console.log(`Consonant`);
+}
