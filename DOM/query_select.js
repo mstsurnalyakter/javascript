d89acfaebe051a6de80d5322216a41ab16898d42
mstsurnalyakter <surnalyms@gmail.com>
2024-01-29 11:55:26 +0600

    dom select added

---
+++ DOM/query_select.js
@@ -0,0 +1,8 @@
+// getElementById()
+// getElementsByTagName()
+// getElementsByClassName()
+
+document.getElementById('heading1').innerHTML = "Heading 1";
+document.getElementsByClassName('heading2')[0].innerHTML = "Heading 2"
+
+document.getElementsByTagName('h1')[2].innerHTML = "Heading 3"
