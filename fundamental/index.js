b018a8e880b4a8da5858b920552b79f325924cc8
mstsurnalyakter <surnalyms@gmail.com>
2024-03-06 21:21:55 +0600

    added some code

---
+++ fundamental/index.js
@@ -0,0 +1,16 @@
+/**
+ * how to create a variable using let and const
+ */
+
+const firstName = 'Mst Surnaly';
+const lastName = 'Akter';
+let season = 'winter';
+
+/**
+ * 6 basic condition <,> === , >=,
+ * && 
+ */
+
+if (condition) {
+
+}
