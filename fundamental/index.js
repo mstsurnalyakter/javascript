6446cca8176a397cbbf5616b124d0a3007e48a52
mstsurnalyakter <surnalyms@gmail.com>
2024-03-07 10:47:40 +0600

    Shortcut

--- fundamental/index.js
+++ fundamental/index.js
@@ -19,3 +19,21 @@ const stringNum = 40 + '';
 // string to number
 const num2 = '40';
 const stringToNum = +num2;
+
+
+let isActive = true;
+
+const showUser = () => console.log('Display User');
+const hideUser = () => console.log('Hide User');
+
+// user && if the left side is true then right side will be execute
+isActive && showUser();
+
+// use || is the left side is false then right side will be execute
+isActive  || hideUser()
+
+// toggle boolean (convert true to false or false to true)
+isActive = !isActive;
+// convert other data type to boolean data
+console.log(!!'hello');
+console.log(!!'');
