e7975586567c66ca851cc2c8488cfef2fd56af97
mstsurnalyakter <surnalyms@gmail.com>
2024-03-07 11:17:20 +0600

    add condition

--- fundamental/storage.js
+++ fundamental/storage.js
@@ -3,7 +3,11 @@
 document.getElementById("submit").addEventListener('click',()=>{
     const key = document.getElementById("key").value;
     const value = document.getElementById("name").value;
-    localStorage.setItem(key, value);
+
+    if (key && value) {
+        localStorage.setItem(key, value);
+
+    }
 
     document.getElementById("key").value = "";
     document.getElementById("name").value = "";
